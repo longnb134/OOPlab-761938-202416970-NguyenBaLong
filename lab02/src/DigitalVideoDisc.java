a001032560c3921574355865f8c4181237b5a555
longnb134 <longnb13406@gmail.com>
2026-04-05 12:01:08 +0700

    Section 8

---
+++ lab02/src/DigitalVideoDisc.java
@@ -0,0 +1,9 @@
+package lab02.src;
+
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+}
