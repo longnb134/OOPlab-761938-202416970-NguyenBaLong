64cca3ee59503670170fae8de09c722e3e43bb11
longnb134 <longnb13406@gmail.com>
2026-04-05 13:23:21 +0700

    Add constructors to DVD class and answer text file (Section 10)

--- lab02/src/DigitalVideoDisc.java
+++ lab02/src/DigitalVideoDisc.java
@@ -7,24 +7,38 @@ public class DigitalVideoDisc {
     private int length;
     private float cost;
 
-
-    public String getTitle() {
-        return title;
+    public DigitalVideoDisc(String title) {
+        super();
+        this.title = title;
     }
 
-    public String getCategory() {
-        return category;
+    public DigitalVideoDisc(String category, String title, float cost) {
+        super();
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
     }
 
-    public String getDirector() {
-        return director;
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        super();
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
     }
 
-    public int getLength() {
-        return length;
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        super();
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
     }
 
-    public float getCost() {
-        return cost;
-    }
+    public String getTitle() { return title; }
+    public String getCategory() { return category; }
+    public String getDirector() { return director; }
+    public int getLength() { return length; }
+    public float getCost() { return cost; }
 }
