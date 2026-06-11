fc442a9d76ef888356d1f24042f22bc0e8222a75
longnb134 <longnb13406@gmail.com>
2026-06-11 17:23:03 +0700

    Clean up UML diagrams: remove source files and fix messy relations

--- AimsProject/Design/GeneratePNGs.ps1
+++
@@ -1,5 +0,0 @@
-$pumlClass = Get-Content -Path "C:\Users\Admin\.vscode\OOPlab-761938-202416970-NguyenBaLong\AimsProject\Design\ClassDiagram.puml" -Raw
-Invoke-WebRequest -Uri "https://kroki.io/plantuml/png" -Method Post -Body $pumlClass -ContentType "text/plain" -OutFile "C:\Users\Admin\.vscode\OOPlab-761938-202416970-NguyenBaLong\AimsProject\Design\ClassDiagram.png"
-
-$pumlUseCase = Get-Content -Path "C:\Users\Admin\.vscode\OOPlab-761938-202416970-NguyenBaLong\AimsProject\Design\UseCaseDiagram.puml" -Raw
-Invoke-WebRequest -Uri "https://kroki.io/plantuml/png" -Method Post -Body $pumlUseCase -ContentType "text/plain" -OutFile "C:\Users\Admin\.vscode\OOPlab-761938-202416970-NguyenBaLong\AimsProject\Design\UseCaseDiagram.png"
